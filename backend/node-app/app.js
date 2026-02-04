d7743e01b4d997db2905f3a0b2d0d322c52a0ba8
Athirah <athirahd645@gmail.com>
2026-02-05 00:35:30 +0800

    websocket support

--- backend/node-app/app.js
+++ backend/node-app/app.js
@@ -1,5 +1,12 @@
 const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: "*" }
+});
 
 app.use(express.json());
 
@@ -8,6 +15,19 @@ app.use(require("./routes/health"));
 app.use(require("./routes/hello"));
 app.use(require("./routes/rsvp"));
 
-app.listen(3001,() => {
-  console.log('Node API running on port 3001');
+io.on("connection", (socket) => {
+  console.log("Client connected:", socket.id);
+
+  socket.on("rsvp", (data) => {
+    console.log("RSVP received:", data);
+    socket.emit("rsvp-status", { status: "received" });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected:", socket.id);
+  });
+});
+
+server.listen(3001,() => {
+  console.log('Node API + WS running on port 3001');
 });
